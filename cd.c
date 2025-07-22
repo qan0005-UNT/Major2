d8267f68b3ac1bc831acb65982bf065c1cf46629
Quan <205038542+qan0005-UNT@users.noreply.github.com>
2025-07-21 19:30:06 -0500

    CD files

---
+++ cd.c
@@ -0,0 +1,33 @@
+#include "cd.h"
+#include <unistd.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <errno.h>
+
+void builtin_cd(const char *path) {
+    if (path == NULL || strcmp(path, "") == 0) {
+        fprintf(stderr, "cd: missing argument\n");
+        return;
+    }
+
+    if (chdir(path) != 0) {
+        perror("cd failed");
+    }
+}
+
+// attempt to change directory
+if(chdir(path) != 0) {
+    perror("cd");
+    return;
+}
+
+// Update the current working directory in the environment
+char cwd[1024];
+if (getcwd(cwd, sizeof(cwd)) != NULL) {
+    if(setenv("PWD", cwd, 1) !+0) {
+        perror("cd: setenv failed");
+    }
+} else {
+    perror("getcwd failed");
+}
